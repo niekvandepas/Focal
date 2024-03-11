9066ba78459db9f064bfec90bf5be5ebdefb458b
Niek van de Pas <niek_pas@live.nl>
2024-03-11 14:17:10 +0100

    Initial Commit

---
+++ Focal/FocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocalApp.swift
+//  Focal
+//
+//  Created by Niek van de Pas on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
