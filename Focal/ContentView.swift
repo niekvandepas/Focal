3abad8fe8b8e61304924019e10b33421b4d6970c
Niek van de Pas <niek_pas@live.nl>
2024-03-11 14:26:55 +0100

    feat: basic timer

--- Focal/ContentView.swift
+++ Focal/ContentView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var timeRemaining = 25 * 60;
+    @State private var timerIsRunning = false;
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(timeRemaining / 60) : \(timeRemaining % 60, specifier: "%02d")")
+                .font(.system(size: 50))
+                .padding()
+            
+            Button(action: {
+                timerIsRunning.toggle()
+            }) {
+                Text(timerIsRunning ? "Pause" : "Start")
+            }
+            .padding()
+            .disabled(timeRemaining == 0)
+
         }
+        .onReceive(timer, perform: { _ in
+            guard timerIsRunning else {return}
+            if timeRemaining > 0{
+                timeRemaining -= 1
+            }
+            else {
+                timeRemaining = 25 * 60
+                timerIsRunning = false
+            }
+        })
         .padding()
     }
 }
