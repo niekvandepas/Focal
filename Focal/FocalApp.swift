13ef4c12a427756143013a535472456291d3815d
Niek van de Pas <niek_pas@live.nl>
2024-03-14 16:17:35 +0100

    feat: ensure the timer window is always open

--- Focal/FocalApp.swift
+++ Focal/FocalApp.swift
@@ -35,10 +35,31 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         if let window = NSApplication.shared.windows.first {
             window.collectionBehavior = .fullScreenNone
         }
+
+        ensureMainWindowIsOpen()
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         true
     }
+
+    /// Ensures the main window (the timer window) is open at app launch.
+    private func ensureMainWindowIsOpen() {
+        // If fewer than 2 windows are open, that means only the menu bar item 'window' is open,
+        // and we need to 'click' the 'New Window' menu bar item to ensure the timer window is open.
+        guard NSApplication.shared.windows.count < 2,
+              let mainMenu = NSApp.mainMenu,
+              let fileMenu = mainMenu.item(withTitle: "File")?.submenu,
+              let newWindowMenuItem = fileMenu.item(withTitle: "New Window"),
+              let action = newWindowMenuItem.action
+        else {
+            return
+        }
+
+        // Programmatically trigger the menu item
+        NSApp.sendAction(action, to: newWindowMenuItem.target, from: nil)
+        newWindowMenuItem.isEnabled = false
+    }
+
 }
 
