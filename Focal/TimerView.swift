e012159fcafb5b82f37a0c3a56f41d0490e31560
Niek van de Pas <niek_pas@live.nl>
2024-03-16 10:14:47 +0100

    refactor(TimerView): move components to variables

--- Focal/TimerView.swift
+++ Focal/TimerView.swift
@@ -15,64 +15,71 @@ struct TimerView: View {
 
     var body: some View {
         VStack {
-            ZStack {
-                Rectangle()
-                    .fill(Color.blue)
-                    .frame(width: 200, height: 200)
-                    .multilineTextAlignment(.center)
-                    .cornerRadius(8)
-
-                VStack {
-                    Text(viewModel.timerState.description.capitalized)
-                        .font(.custom("Inter", size: 18))
-                        .padding(.bottom, -20)
-
-                    Text("\(viewModel.timeRemaining / 60):\(viewModel.timeRemaining % 60, specifier: "%02d")")
-                        .font(.custom("Inter", size: 50))
-                        .padding()
-                        .foregroundStyle(.primaryButton)
-
-                }
-                .onAppear {
-                    hotkey.keyDownHandler = {
-                        viewModel.toggleTimer()
-                        NSApp.activate()
-                    }
-                }
-            }
-
-            HStack {
-                Button(action: {
-                    viewModel.toggleTimer()
-                }) {
-                    Text(viewModel.timerIsRunning ? "Pause" : "Start")
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 20)
-                }
-                .disabled(viewModel.timeRemaining == 0)
-                .keyboardShortcut(" ")
-                .font(.headline)
-                .foregroundColor(.white)
-                .background(.primaryButton)
-                .cornerRadius(2)
-
-                Button(action: {
-                    viewModel.resetTimer()
-                }) {
-                    Text("Reset")
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 20)
-                        .foregroundStyle(.primaryButton)
-                }
-                .keyboardShortcut(KeyEquivalent("r"), modifiers: [.command])
-                .font(.headline)
-                .background(.white)
-                .cornerRadius(2)
-            }
-//            .background(.green)
-            .frame(width: 220)
+            timerRect
+            buttons
         }
 //        .background(.yellow)
+    }
+
+    var timerRect: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: 200, height: 200)
+                .multilineTextAlignment(.center)
+                .cornerRadius(8)
+
+            VStack {
+                Text(viewModel.timerState.description.capitalized)
+                    .font(.custom("Inter", size: 18))
+                    .padding(.bottom, -20)
+
+                Text("\(viewModel.timeRemaining / 60):\(viewModel.timeRemaining % 60, specifier: "%02d")")
+                    .font(.custom("Inter", size: 50))
+                    .padding()
+                    .foregroundStyle(.primaryButton)
+
+            }
+            .onAppear {
+                hotkey.keyDownHandler = {
+                    viewModel.toggleTimer()
+                    NSApp.activate()
+                }
+            }
+        }
+    }
+
+    var buttons: some View {
+        HStack {
+            Button(action: {
+                viewModel.toggleTimer()
+            }) {
+                Text(viewModel.timerIsRunning ? "Pause" : "Start")
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 20)
+            }
+            .disabled(viewModel.timeRemaining == 0)
+            .keyboardShortcut(" ")
+            .font(.headline)
+            .foregroundColor(.white)
+            .background(.primaryButton)
+            .cornerRadius(2)
+
+            Button(action: {
+                viewModel.resetTimer()
+            }) {
+                Text("Reset")
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 20)
+                    .foregroundStyle(.primaryButton)
+            }
+            .keyboardShortcut(KeyEquivalent("r"), modifiers: [.command])
+            .font(.headline)
+            .background(.white)
+            .cornerRadius(2)
+        }
+//            .background(.green)
+        .frame(width: 220)
     }
 }
 
