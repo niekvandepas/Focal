91ba99f83deb713db49f6eaf38b98c27ff06a9ab
Niek van de Pas <niek_pas@live.nl>
2024-03-19 13:18:55 +0100

    feat: add initial iOS target

---
+++ Focal iOS/Focal_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Focal_iOSApp.swift
+//  Focal iOS
+//
+//  Created by Niek van de Pas on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Focal_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
