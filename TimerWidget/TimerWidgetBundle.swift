fa5f1ce3a27c9f217737a143ff3575a3c0a9a05a
Niek van de Pas <niek_pas@live.nl>
2024-03-29 12:15:28 +0100

    feat: Apple example widget

---
+++ TimerWidget/TimerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TimerWidgetBundle.swift
+//  TimerWidget
+//
+//  Created by Niek van de Pas on 28/03/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TimerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TimerWidget()
+    }
+}
