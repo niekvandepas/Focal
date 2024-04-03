02ce196715be63375760ee37efc7bce6efdeae13
Niek van de Pas <n.vandepas@uu.nl>
2024-04-03 16:43:20 +0200

    fix: Widgets not working because I accidentally a hardcoded string
    (This took me several days)

--- Constants.swift
+++ Constants.swift
@@ -17,5 +17,5 @@ struct Constants {
     static let UD_GROUP_NAME = "group.com.Focal"
     static let UD_TIME_REMAINING = "TimeRemaining"
     static let UD_TIMER_IS_RUNNING = "TimerIsRunning"
-    static let UD_TIMER_STATE = "TimerIsRunning"
+    static let UD_TIMER_STATE = "TimerState"
 }
