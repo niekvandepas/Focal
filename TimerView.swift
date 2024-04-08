bb221eb9f588f06033c62616cc64ae1040cfc53a
Niek van de Pas <n.vandepas@uu.nl>
2024-04-08 08:50:15 +0200

    style

--- TimerView.swift
+++ TimerView.swift
@@ -15,14 +15,14 @@ import UserNotifications
 struct TimerView: View {
     @StateObject var timerViewModel = TimerViewModel.shared
     @ObservedObject var settingsManager = SettingsManager.shared
-
+    
     var body: some View {
 #if os(macOS)
         let buttonFrameWidth = 200.0
 #else
         let buttonFrameWidth = 250.0
 #endif
-
+        
         VStack {
             Spacer()
             timerRect
@@ -34,7 +34,7 @@ struct TimerView: View {
                 .padding(.bottom)
         }
     }
-
+    
     var timerRect: some View {
         // work:    .workBlue
         // rest:    .breakGreen
@@ -45,7 +45,7 @@ struct TimerView: View {
 #else
         let timerStateLabelFontSize = 26.0
 #endif
-
+        
 #if os(macOS)
         let timerTimeLeftFontSize = 50.0
 #else
@@ -61,8 +61,8 @@ struct TimerView: View {
 #else
         let timerRectangleHeight = 250.0
 #endif
-
-//        TODO fix this to check if the text is empty, which it is by default because of placeholder stuff
+        
+        //        TODO fix this to check if the text is empty, which it is by default because of placeholder stuff
         let timerLabelText = {
             switch timerViewModel.timerState {
             case .work:
@@ -70,11 +70,11 @@ struct TimerView: View {
             case .rest:
                 settingsManager.optionalTimerBreakLabel == "" ? "Break" : settingsManager.optionalTimerBreakLabel
             }
-
+            
         }()
-
+        
         let timerText = settingsManager.showTimeLeft ? timerViewModel.timeRemainingFormatted : timerLabelText
-
+        
         return ZStack {
             Rectangle()
                 .fill(timerSquareColor)
@@ -83,7 +83,7 @@ struct TimerView: View {
                 .cornerRadius(8)
                 .shadow(radius: 1, x: 5, y: 5)
                 .padding(.bottom, 10)
-
+            
             VStack {
                 if settingsManager.showTimeLeft {
                     Text(timerLabelText)
@@ -91,27 +91,27 @@ struct TimerView: View {
                         .padding(.bottom, -20)
                         .foregroundStyle(.black)
                 }
-
+                
                 Text(timerText)
                     .font(.custom("Inter", size: timerTimeLeftFontSize))
                     .padding()
                     .foregroundStyle(.primaryButton)
-
+                
             }
         }
     }
-
+    
     var buttons: some View {
-        #if os(iOS)
+#if os(iOS)
         let notificationFeedbackGenerator = UINotificationFeedbackGenerator()
         let mediumFeedbackGenerator = UIImpactFeedbackGenerator(style: .medium)
-        #endif
-
+#endif
+        
         let startPauseButton = Button(action: {
-            #if os(iOS)
+#if os(iOS)
             timerViewModel.timerIsRunning ? mediumFeedbackGenerator.impactOccurred() : notificationFeedbackGenerator.notificationOccurred(.success)
-            #endif
-
+#endif
+            
             timerViewModel.toggleTimer()
 #if os(macOS)
             if settingsManager.hideAppOnTimerStart && timerViewModel.timerIsRunning {
@@ -131,18 +131,18 @@ struct TimerView: View {
                 .bold()
                 .contentShape(Rectangle()) // This is needed to have the entire button actuate
         }
-        .customFocus()
-        .disabled(timerViewModel.timeRemaining == 0)
-        .keyboardShortcut(" ")
-        .foregroundColor(.white)
-        .background(.primaryButton)
-        .border(.black, width: 2)
-        .background( // rounded border
-            RoundedRectangle(cornerRadius: 0)
-                .offset(x: 3, y: 3)
-                .fill(.black)
-        )
-
+            .customFocus()
+            .disabled(timerViewModel.timeRemaining == 0)
+            .keyboardShortcut(" ")
+            .foregroundColor(.white)
+            .background(.primaryButton)
+            .border(.black, width: 2)
+            .background( // rounded border
+                RoundedRectangle(cornerRadius: 0)
+                    .offset(x: 3, y: 3)
+                    .fill(.black)
+            )
+        
         let resetButton = Button(action: {
             timerViewModel.resetTimer()
         }) {
@@ -152,27 +152,27 @@ struct TimerView: View {
                 .foregroundStyle(.primaryButton)
                 .contentShape(Rectangle()) // This is needed to have the entire button actuate
         }
-        .disabled(timerViewModel.timerIsFull)
-        .customFocus()
-        .keyboardShortcut(KeyEquivalent("r"), modifiers: [.command])
-        .background(.white)
-        .border(.black, width: 2)
-        .background( // rounded border
-            RoundedRectangle(cornerRadius: 0)
-                .offset(x: 3, y: 3)
-                .fill(.black)
-        )
-        .overlay(timerViewModel.timerIsFull ? GeometryReader { geometry in
-            // Draw a red X
-            Path { path in
-                path.move(to: CGPoint(x: 0, y: 0))
-                path.addLine(to: CGPoint(x: geometry.size.width, y: geometry.size.height))
-                path.move(to: CGPoint(x: geometry.size.width, y: 0))
-                path.addLine(to: CGPoint(x: 0, y: geometry.size.height))
-            }
-            .stroke(Color.red, lineWidth: 5)
-        } : nil)
-
+            .disabled(timerViewModel.timerIsFull)
+            .customFocus()
+            .keyboardShortcut(KeyEquivalent("r"), modifiers: [.command])
+            .background(.white)
+            .border(.black, width: 2)
+            .background( // rounded border
+                RoundedRectangle(cornerRadius: 0)
+                    .offset(x: 3, y: 3)
+                    .fill(.black)
+            )
+            .overlay(timerViewModel.timerIsFull ? GeometryReader { geometry in
+                // Draw a red X
+                Path { path in
+                    path.move(to: CGPoint(x: 0, y: 0))
+                    path.addLine(to: CGPoint(x: geometry.size.width, y: geometry.size.height))
+                    path.move(to: CGPoint(x: geometry.size.width, y: 0))
+                    path.addLine(to: CGPoint(x: 0, y: geometry.size.height))
+                }
+                .stroke(Color.red, lineWidth: 5)
+            } : nil)
+        
         if #available(macOS 14.0, *) {
             return HStack {
                 startPauseButton
@@ -191,9 +191,9 @@ struct TimerView: View {
             }
             .buttonStyle(MyButtonStyle())
         }
-
+        
     }
-
+    
     var sessionMarkers: some View {
         HStack {
             ForEach(0..<5) { index in
