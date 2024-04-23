2fb5594dda4300f0215dbb4af018a66f783ebc6b
Niek van de Pas <n.vandepas@uu.nl>
2024-04-23 14:14:21 +0200

    feat: settings link in menu bar menu

--- Focal/AppMenu.swift
+++ Focal/AppMenu.swift
@@ -32,6 +32,10 @@ struct AppMenu: View {
         NSApplication.shared.terminate(nil)
     }
 
+    func showSettings() {
+        NSApp.sendAction(Selector(("showSettingsWindow:")), to: nil, from: nil)
+    }
+
     var body: some View {
         Button(action: handleStartOrPause, label: { timerViewModel.timerIsRunning ? Text("Pause Timer") : Text("Start Timer") })
             .keyboardShortcut(" ", modifiers: [])
@@ -44,6 +48,15 @@ struct AppMenu: View {
 
         Button(action: bringAppToFront, label: { Text("Open Focal") })
             .keyboardShortcut(KeyEquivalent("o"), modifiers: [.command])
+
+        if #available(macOS 14.0, *) {
+            SettingsLink()
+                .keyboardShortcut(KeyEquivalent(","), modifiers: [.command])
+        }
+        else {
+            Button(action: showSettings, label: { Text("Settings...") })
+                .keyboardShortcut(KeyEquivalent(","), modifiers: [.command])
+        }
 
         Button(action: quitApp, label: { Text("Quit Focal") })
             .keyboardShortcut(KeyEquivalent("q"), modifiers: [.command])
