16231248418d6a220ec63cf3304d2488db346caa
Niek van de Pas <n.vandepas@uu.nl>
2024-12-25 12:08:18 +0100

    fix(strict-concurrency): ensure UI updates on main actor for AppDelegate methods

--- Focal/AppDelegate.swift
+++ Focal/AppDelegate.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import UserNotifications
 
-final class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDelegate {
+final class AppDelegate: NSObject, NSApplicationDelegate, @preconcurrency UNUserNotificationCenterDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         if let window = NSApplication.shared.windows.first {
             window.collectionBehavior = .fullScreenNone
@@ -24,6 +24,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCent
         ensureMainWindowIsOpen()
     }
 
+    @MainActor
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         if response.actionIdentifier == "START_NEXT_TIMER" {
             TimerViewModel.shared.startTimer()
@@ -32,6 +33,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCent
     }
 
     /// Opens the main window (the timer window) if it is not already open
+    @MainActor
     private func ensureMainWindowIsOpen() {
         // If fewer than 2 windows are open, that means only the menu bar item 'window' is open,
         // and we need to 'click' the 'New Window' menu bar item to ensure the timer window is open.
@@ -50,6 +52,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCent
     }
 
     /// Disables window rezising for all application windows
+    @MainActor
     private func disableWindowResizing() {
         NSApp.windows.forEach { $0.styleMask.remove(.resizable) }
     }
